e67a5b936240fcb129e796108aec31c14a83b0d3
joyhuangg <joyhuangg@gmail.com>
2018-07-30 14:50:49 -0700

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,12 @@
+require 'pry'
 def alphabetize(arr)
-  # code here
+  alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+  arr.sort do |a, b|
+    # binding.pry
+    i = 0
+    while a[i] == b[i]
+      i += 1
+    end
+    alphabet.index(a[i]) <=> alphabet.index(b[i])
+  end
 end
